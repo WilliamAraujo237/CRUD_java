6e5cc0d0f0e354f29885f82a2715678d481d2368
William Araújo <william237.araujo@gmail.com>
2023-05-16 20:36:13 -0300

    Criando o Read (LEITURA) das informações do banco de dados

--- Crud_Java/src/br/com/comida/dao/comidaDAO.java
+++ Crud_Java/src/br/com/comida/dao/comidaDAO.java
@@ -2,6 +2,10 @@ package br.com.comida.dao;
 
 import java.sql.Connection;
 import java.sql.Date;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
 import com.mysql.jdbc.PreparedStatement;
 import br.com.comidas.comidas;
 import br.com.comidas.factory.ConnectionFactory;
@@ -16,6 +20,8 @@ public class comidaDAO {
  * */
 	
 	public void SAVE(comidas Conection) {
+
+		
 		String sql = "INSERT INTO COMIDA (NOME,VALOR,CRIADOR,DATA) VALUE (?,?,?,?)";
 		
 		Connection comm = null;
@@ -55,5 +61,60 @@ public class comidaDAO {
 				e2.printStackTrace();
 			}
 		}
+	}
+	
+	public List<comidas> getComidas() {
+		String sql = "SELECT * FROM COMIDA";
+		
+		List<comidas> comidas = new ArrayList<comidas>();
+		
+		Connection conn = null;
+		PreparedStatement pstm = null;
+		//classe que vai recuperar dados do banco   *****SELECT***
+		ResultSet rset = null;
+		
+		try {
+			//Criando uma conexão com o banco de dados
+			conn = ConnectionFactory.CreateConnectionToMYSQL();
+			//criando um prepareStatement para executar a Query
+			pstm = (PreparedStatement) conn.prepareStatement(sql);
+			
+			//Variavel para armazenar uma Array com resultados da Query
+			rset  = pstm.executeQuery();
+			
+			while (rset.next()) {
+				comidas comida = new comidas(sql, null, sql);
+				
+				//recuperando resultados CRIADOR
+				comida.setCriador(rset.getString("CRIADOR"));
+				//recuperando resultados NOME
+				comida.setNome(rset.getString("NOME"));
+				//recuperando resultados DATA
+				comida.setData(rset.getDate("DATA"));
+				//recuperando resultados VALOR
+				comida.setValor(rset.getDouble("VALOR"));
+				
+				comidas.add(comida);
+			}
+			
+			
+			} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if (rset != null) {
+					rset.close();
+				}
+				if (pstm != null) {
+					pstm.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return comidas;
 	}
 }
